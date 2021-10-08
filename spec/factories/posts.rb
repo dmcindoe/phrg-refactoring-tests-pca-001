8b2a99d63f50a15cb6a37e132f6daa7b73604cee
Andrew McIndoe <amcindoe@powerhrg.com>
2021-10-08 09:46:01 -0400

    commit tree a6880cf7491feaefa4f51cf9838e80481b4517d2

---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    title "Won't Believe Truth of Weight loss"
+    content "Good stuff here" * 250
+    summary "This was Nice I liked it"
+    category "Fiction"
+  end
+end  
